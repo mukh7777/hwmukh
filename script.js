8caa5b941e897ad0ffb61f1f4b88b64159f959c7
mukh <mukh@gmail>
2023-02-22 17:17:15 +0500

    first

---
+++ script.js
@@ -0,0 +1,22 @@
+var age = prompt("сколько вам лет");
+var name = prompt("как вас зовут");
+
+if (age >= 0 && age <= 18 ) {
+  alert('вы еще молоды вам нужно учится');
+} else if (age >= 19  && age <=  50) {
+  alert('вам нужно работать');
+}
+else if (age >= 50  && age <=  59) {
+    alert('вам скоро на пенсию');
+  }
+  else if (age >= 59  && age <=  100) {
+    alert('вы пенсионер');
+  }
+  
+//   var time =prompt( 'какое время');
+//   if ( time >=12 && time <= 6 ){
+    
+//   }
+  
+  
+
